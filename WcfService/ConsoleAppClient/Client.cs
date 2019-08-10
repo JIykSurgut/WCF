4d35b07d54435355c0084df9a043568d99c8935a
sbis86 <sbis86@mail.ru>
2019-08-10 19:29:10 +0500

    Пример 1

---
+++ WcfService/ConsoleAppClient/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace ConsoleAppClient
+{
+    [ServiceContract]
+    public interface IStockService
+    {
+        [OperationContract]
+        double GetPrice(string ticker);
+    }
+
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<IStockService> myChannelFactory =
+                new ChannelFactory<IStockService>(new BasicHttpBinding(), new EndpointAddress("http://localhost:8000/EssentialWCF"));
+
+            IStockService wcfClient = myChannelFactory.CreateChannel();
+
+            double p = wcfClient.GetPrice("msft");
+            Console.WriteLine("Price:{0}",p);
+        }
+    }
+}
