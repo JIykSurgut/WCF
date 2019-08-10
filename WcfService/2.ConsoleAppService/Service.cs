4715aee01ab2ca0fe050e91ea50e6c8027acb734
sbis86 <sbis86@mail.ru>
2019-08-10 23:04:49 +0500

    0x01

---
+++ WcfService/2.ConsoleAppService/Service.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace EssentialWCF
+{
+    [ServiceContract]
+    public interface IStockService
+    {
+        [OperationContract]
+        double GetPrice(string ticker);
+    }
+
+    public class StockService : IStockService
+    {
+        public double GetPrice(string ticker)
+        {
+            return 94.85;
+        }
+    }
+
+    class Service
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost serviceHost = new ServiceHost(typeof(StockService));
+            serviceHost.Open();
+
+            Console.WriteLine("Для завершения нажмите <ENTER>.\n\n");
+            Console.ReadLine();
+            serviceHost.Close();
+        }
+    }
+}
